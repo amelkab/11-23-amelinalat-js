2d66959c4a948f890b76084ffe09b95b8cc58728
amelkab <amelkabobr0@gmail.com>
2024-09-21 11:07:49 +0300

    новые sm

---
+++ sm1/main.js
@@ -0,0 +1,9 @@
+let x = 6;
+let y = 2;
+let z = "test";
+
+console.log(`${x} + ${y}=${x+y}`);
+console.log(`${x} - ${y}=${x-y}`);
+console.log(`${x} * ${y}=${x*y}`);
+console.log(`${x} / ${y}=${x/y}`);
+console.log(x/c)
